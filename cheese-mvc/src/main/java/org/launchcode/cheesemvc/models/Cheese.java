1465c226c457e442f8bfe3a543b393a4ccbeb3cf
Judy@LAPTOP <judylight2001@gmail.com>
2018-06-03 16:52:04 -0700

    added remove function and created Cheese class

--- cheese-mvc/src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ cheese-mvc/src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -4,10 +4,17 @@ public class Cheese {
 
     private String name;
     private String description;
+    private int cheeseId;
+    private static int nextId = 1;
 
     public Cheese(String name, String description) {
         this.name = name;
         this.description = description;
+    }
+
+    public Cheese() {
+        cheeseId = nextId;
+        nextId++;
     }
 
     public String getName() {
